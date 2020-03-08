dc24833fa6573fbe35ebc6725a0e506b84196c23
Username <exa@mp.le>
2020-03-08 08:47:27 -0700

    Added HTML, CSS, SASS folders, got nodes

--- Project 1/js/main.js
+++ Project 1/js/main.js
@@ -1,3 +1,43 @@
+// Задание 7 по проекту
+
+// · Получить кнопку "Начать расчет" через id
+let start = document.getElementById('start');
+
+// · Получить все блоки в правой части программы через классы (которые имеют класс название-value,
+// начиная с <div class="budget-value"></div> и заканчивая <div class="yearsavings-value"></div>)
+let budgetValue = document.getElementsByClassName('budget-value'),
+    daybudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value');
+
+// · Получить поля(input) c обязательными расходами через класс. (class=”expenses-item”)
+let expensesInput = document.getElementsByClassName('expenses-item');
+
+// · Получить кнопки “Утвердить” и “Рассчитать” через Tag, каждую в своей переменной. 
+let confirmBtn = document.getElementsByClassName('expenses-item-btn'),
+    calculateBtn = document.getElementsByClassName('count-budget-btn');
+
+// · Получить поля для ввода необязательных расходов (optionalexpenses-item) при помощи querySelectorAll
+let optionalexpensesInput = document.querySelectorAll('.optionalexpenses-item');
+
+// · Получить оставшиеся поля через querySelector (статьи возможного дохода, чекбокс, сумма, процент, год, месяц, день)
+let income = document.querySelector('#income'),
+    savings = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+// 3) Проверить, чтобы все работало и не было ошибок в консоли
+// 4) Добавить папку с уроком на свой GitHub
+// Вопросы к этому заданию
+// Как можно изменить размер шрифта элемента при помощи JS?
+
 'use strict';
 
 // создать 2 переменные (money и time), которые будут получать данные от пользователя:
